70c6cac9806fa7de7b7f5ef1ade8b52a80ffb8f9
vaibhavbshete <30766384+vaibhavbshete@users.noreply.github.com>
2022-09-09 18:51:15 +0530

    add expenses chart component solution

---
+++ expenses-chart-component/assets/script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    fetch('data.json').then(rawdata=>
+      rawdata.json()).then(data=>{
+      // console.log(data);
+      let amounts = data.map(daydata=> parseFloat(daydata.amount));
+      maxAmount =Math.max(...amounts);
+      const days = Array('mon','tue','wed','thu','fri','sat','sun');
+      days.forEach(function(day){
+        let daydata = data.filter(dayinfo=>dayinfo.day==day).pop();
+        // console.log(daydata);
+        let height = daydata.amount/maxAmount*100;
+        let daybar=document.querySelector('[data-day='+daydata.day);
+        daybar.style.height=height+"%";
+        if(daydata.amount==maxAmount){
+          daybar.classList.add('max');
+        }
+        daybar.querySelector('.bar-text').innerText='$'+daydata.amount;
+      });
+    });
+  })
