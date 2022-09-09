8e8432b22f82a807f14e82874b0f2fcb74a0b26b
vaibhavbshete <30766384+vaibhavbshete@users.noreply.github.com>
2022-09-09 18:56:26 +0530

    relative links in expenses chart

--- expenses-chart-component/assets/script.js
+++ expenses-chart-component/assets/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded',()=>{
-    fetch('data.json').then(rawdata=>
+    fetch('./data.json').then(rawdata=>
       rawdata.json()).then(data=>{
       // console.log(data);
       let amounts = data.map(daydata=> parseFloat(daydata.amount));
