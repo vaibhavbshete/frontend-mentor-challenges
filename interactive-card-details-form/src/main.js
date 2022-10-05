8956dd7ad79eba7779168878ff6a58a0606c7166
vaibhavbshete <30766384+vaibhavbshete@users.noreply.github.com>
2022-10-05 18:03:08 +0530

    validations and tighter bindings

--- interactive-card-details-form/src/main.js
+++ interactive-card-details-form/src/main.js
@@ -28,12 +28,17 @@ createApp({
             },
             cardSaved: false,
             submitCheck: false,
-            formHasErrors:false
+            formHasErrors: false,
+            numCurPos: 0,
+            numSpacesBeforeCursor:[]
         }
     },
     // watch: {
-    //     cardNum(value) {
-    //         console.log(value);
+    //     card: {
+    //         handler(newVal, oldVal) {
+    //             console.log(newVal.number.value);
+    //         },
+    //         deep:true
     //     }
     // },
     computed: {
@@ -66,26 +71,31 @@ createApp({
         },
        
     },
-    
-    methods: {
-        onCardNumInput(event) {
+    beforeUpdate() {
+        // record cursor position
+        if (document.activeElement == document.forms.cardForm.number) {
+            this.numCurPos = document.activeElement.selectionStart;
+            this.numSpacesBeforeCursor = [...document.activeElement.value.matchAll(/\s/g)]
+                .map(match => match.index)
+                .filter(index => index < this.numCurPos).length;
+        }
+    },
+    updated() {
+        // reupdate cursor position
+        if (document.activeElement == document.forms.cardForm.number) {
+            let cursorNumDiff = [...this.card.number.value.matchAll(/\s/g)]
+                .map(match => match.index)
+                .filter(index => index < this.numCurPos).length - this.numSpacesBeforeCursor;
 
-            // console.log(event);
-            let cardInputTxt = event.target;
+            document.forms.cardForm.number.setSelectionRange(this.numCurPos + cursorNumDiff, this.numCurPos + cursorNumDiff);
+        }
+    },
+    methods: {
+        modInputWSpaces(event) {
+
             let cardNum = event.target.value;
-            // console.log(parseInt(cardNum));
             cardNum = cardNum.replace(/\s*/g, '');
-            // validate
-            // if (isNaN(cardNum)) {
-            //     // this.kk = "Only numbers allowed";
-            //     // console.error(");
-            //     cardInputTxt.setCustomValidity("Only numberrrs allowed");
-            //     // return;
-            // }
-            // else {
-            //     cardInputTxt.setCustomValidity("");
-            // }
-            // display validation message
+        
 
             // modify with spaces
             let wspaces = cardNum.matchAll(/.{1,4}/g);
@@ -95,81 +105,177 @@ createApp({
             }
             this.card.number.value = wspaces;
             this.$forceUpdate();
-            // card.number.value = event.target.value
-            return;
         },
-        validate(item, itemkey, activeCheck=false) {
-            // let items;
-            // if (typeof item.value != 'array') {
-            //     items=[item]
-            // }
-            // else {
-            //     items=item
-            // }
-            // items.forEach(item => {
-            item.error = this.validations()[itemkey](item, activeCheck);
-            // console.log(itemkey,'error',item.error);
-            if (item.error !== "") {
+        validate(input, activeCheck = false) {
+            [...document.forms.cardForm.elements].forEach(element => element.setCustomValidity(''));
+            let fieldNameArr = input.dataset.validationField.split('.');
+            let field = this;
+            fieldNameArr.forEach(segment => field = field[segment]);
+                field.error = this['validation_'+input.dataset.validationFunction](input, activeCheck);
+            if (field.error !== "") {
                 this.formHasErrors = true;
-                // console.log(this.formHasErrors);
             }
-            // })
+            input.setCustomValidity(field.error);
         },
         onSubmit() {
-            // console.log('called?');
             this.submitCheck = true;
             this.formHasErrors = false;
-            for (const [key, value] of Object.entries(this.card)) {
-                // console.log(key);
-                this.validate(value,key,true);
-                // value.error = "Error hai "+key+" ka bhaiya!";
-                // console.log(value);
+            for(const input of document.forms.cardForm.querySelectorAll('input')){
+                this.validate(input,true);
             }
-            // console.log('b');
             if (!this.formHasErrors) {
-                // console.log('a');
                 this.cardSaved = true;
+            }
+        },
+        validation_expDateYear(yearInput, activeCheck) {
+            // console.log(this);
+            let errMsg = "";
+            let yearVal = yearInput.value;
+            if (yearVal.length < 1 && activeCheck) {
+                return "Can't be blank";
+            }
+            if (isNaN(yearVal)) {
+                errMsg += "Number only ";
+            }
+            else if (this.card.expDate.month.value && activeCheck) {
+                // console.log('checking if fututre');
+                let now = new Date();
+                let expDate = new Date( this.card.expDate.month.value + ' 1 '+yearVal );
+                if ( now > expDate) {
+                    errMsg += "Only future date ";
+                }
+            }
+            return errMsg;
+        },
+        validation_name(nameInput) {
+            if (nameInput.value.length < 1) {
+                return "Can't be blank";
+            }
+            return '';  
+        },
+        validation_number(numberInput,activeCheck) {
+            let errMsg = "";
+            let numSansSpaces = numberInput.value.replaceAll(/\s*/g, '');
+            if (numSansSpaces.length < 1) {
+                return "Can't be blank";
+            }
+            if (isNaN(numSansSpaces)) {
+                errMsg = "Wrong format, numbers only";
+                return errMsg;
+            }
+            if (numSansSpaces.length < 16 && activeCheck) {
+                errMsg+="16 digit number required"
+            }
+            return errMsg;
+        },
+        validation_cvc(cvc,activeCheck) {
+            if (cvc.value.length < 1) {
+                return "Can't be blank";
+            }
+            if (isNaN(cvc.value)){
+              return "Numbers only"
+            }
+            if (cvc.value.length != 3 && activeCheck) {
+                return "3 digits only"
+            }
+            else return '';
+        },
+        validation_expDateMonth(monthInput, activeCheck) {
+            let errMsg = "";
+            let monthVal = monthInput.value;
+            if (monthVal.length < 1 && activeCheck) {
+                return "Can't be blank";
+            }
+            if (isNaN(monthVal)) {
+                errMsg += "Number only ";
+            }
+            else if (monthVal > 12 || monthVal < 1) {
+                errMsg += "Only in 1-12 range ";
+            }
+            else if (this.card.expDate.year.value && activeCheck) {
+                // console.log('checking if fututre');
+                let now = new Date();
+                let expDate = new Date(monthVal + ' 1 ' + this.card.expDate.year.value);
+                if (now > expDate) {
+                    errMsg += "Only future date ";
+                }
+                return errMsg;
             }
         },
         validations(){
             return {
-                name() {
+                name(nameInput) {
+                    if (nameInput.value.length < 1) {
+                        return "Can't be blank";
+                    }
                     return '';  
                 },
-                number(numberTxt, submitCheck) {
+                number(numberInput,activeCheck) {
                     let errMsg = "";
-                    let numSansSpaces = numberTxt.value.replaceAll(/\s*/g, '');
+                    let numSansSpaces = numberInput.value.replaceAll(/\s*/g, '');
+                    if (numSansSpaces.length < 1) {
+                        return "Can't be blank";
+                    }
                     if (isNaN(numSansSpaces)) {
                         errMsg = "Wrong format, numbers only";
                         return errMsg;
                     }
-                    if (numSansSpaces.length < 16) {
+                    if (numSansSpaces.length < 16 && activeCheck) {
                         errMsg+="16 digit number required"
                     }
                     return errMsg;
                 },
-                cvc(cvc,submitCheck) {
-                    // console.log(this);
+                cvc(cvc,activeCheck) {
+                    if (cvc.value.length < 1) {
+                        return "Can't be blank";
+                    }
                     if (isNaN(cvc.value)){
                       return "Numbers only"
                     }
-                    if (cvc.value.length != 3 && submitCheck) {
-                        return "Only 3 digits allowed"
+                    if (cvc.value.length != 3 && activeCheck) {
+                        return "3 digits only"
                     }
                     else return '';
                 },
                 expDate(dateObj) {
                     let errMsg = "";
-                    // console.log(dateObj);x
+                    if (dateObj.month.value.length < 1 || dateObj.year.value.length <1 ) {
+                        return "Can't be blank";
+                    }
                     if (isNaN(dateObj.month.value) || isNaN(dateObj.year.value)){
                         errMsg += "Numbers only";
-                        // return errMsg;
                     }
                     else if (dateObj.month.value > 12 || dateObj.month.value < 1) {
                         errMsg += "Numbers only in 1-12 range";
-                        // return errMsg;
                     }
                     return errMsg;
+                },
+                expDateMonth(monthInput, activeCheck) {
+                    let errMsg = "";
+                    let monthVal = monthInput.value;
+                    if (monthVal.length < 1 && activeCheck) {
+                        return "Can't be blank";
+                    }
+                    if (isNaN(monthVal)) {
+                        errMsg += "Number only ";
+                    }
+                    else if (monthVal > 12 || monthVal < 1) {
+                        errMsg += "Only in 1-12 range ";
+                    }
+                },
+                expDateYear(yearInput, activeCheck, inst = this) {
+                    console.log(inst);
+                    let errMsg = "";
+                    let yearVal = yearInput.value;
+                    if (yearVal.length < 1 && activeCheck) {
+                        return "Can't be blank";
+                    }
+                    if (isNaN(yearVal)) {
+                        errMsg += "Number only ";
+                    }
+                    else if (inst.card.expDate.month.value && ((new Date()) < (new Date(yearVal +' '+inst.card.expDate.month.value+ ' 1')))) {
+                        errMsg += "Only future date ";
+                    }
                 }
                 
             }          
