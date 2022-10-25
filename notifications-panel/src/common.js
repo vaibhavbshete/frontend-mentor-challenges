79afe5de58c8d5946f11fd4f65cac91f66571b85
vaibhavbshete <30766384+vaibhavbshete@users.noreply.github.com>
2022-10-26 00:28:16 +0530

    wip: building out the desktop version

---
+++ notifications-panel/src/common.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const crypto = require('crypto');
+const getHash = (content, limit = -1) => {
+    var hash = crypto.createHash('md4');
+    //passing the data to be hashed
+    let data = hash.update(content, 'utf-8');
+    //Creating the hash in the required format
+    let gen_hash = data.digest('hex');
+    if (limit > -1) {
+        gen_hash = gen_hash.substring(0, limit);
+    }
+    return gen_hash;
+};
+const getFileHash = function (filepath) {
+    var content = getHash(fs.readFileSync(path.resolve(__dirname, filepath)), 20);
+    return content;
+};
+exports.getFileHash= getFileHash 
--- notifications-panel/src/main.js
+++ notifications-panel/src/main.js
@@ -4,17 +4,61 @@ import style from "./style.sass";
 createApp({
     data() {
         return {
-            message: 'HW',
-            count:7
+            message:"lee",
+            notifications: [
+                {
+                    sender: 'Mark Webber',
+                    text: 'reacted to your recent post',
+                    subject: 'My first tournament today!',
+                    time: '1m ago',
+                    read:false
+                },
+                
+                {
+                    sender: 'Angela Gray',
+                    text: 'followed you',
+                    time: '5m ago',
+                    read:false
+                },
+                {
+                    sender: 'Jacob Thompson',
+                    text: 'has joined your group',
+                    subject: 'Chess Club',
+                    time: '1 day ago',
+                    read:false
+                },
+                
+                
+                
+                {
+                    sender: 'Rizky Hasanuddin',
+                    text: 'sent you a private message',
+                    time: '5 days ago',
+                    read:true,
+                    preview: "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
+                    previewType:"text"
+                },
+                {
+                    sender: 'Kimberly Smith',
+                    text: 'commented on your picture',
+                    time: '1 week ago',
+                    read:true
+                },
+                {
+                    sender: 'Nathan Peterson',
+                    text: 'reacted to your recent post',
+                    subject: '5 end-game strategies to increase your win rate',
+                    time: '2 weeks ago',
+                    read:true
+                },
+                {
+                    sender: 'Anna Kim',
+                    text: 'left the group',
+                    subject: ' Chess Club',
+                    time: '2 weeks ago',
+                    read:true
+                },
+            ]
         }
-    },
-    mounted() {
-        setInterval(advance, 500);
-        const self = this;   
-        function advance() {
-
-            self.count++
-        }
-
     },
 }).mount('#app');
