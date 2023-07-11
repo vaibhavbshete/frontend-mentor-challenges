e256985f09454c9998e75e21a662aed6488e11ec
vaibhavbshete <30766384+vaibhavbshete@users.noreply.github.com>
2023-07-11 20:52:05 +0530

    setup react environment

---
+++ multi-step-form/webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: '.',
+  
+       hot: true,
+      },
+    entry: './src/form.js',
+    output: {
+        path: __dirname + '/built-assets',
+        filename: 'form.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', ['@babel/preset-react', { runtime: 'automatic' }]],
+                    }
+                }
+            }
+        ]
+    },
+    // plugins: [
+    //     new HtmlWebpackPlugin({
+    //       title: 'Hot Module Replacement',
+    //     }),
+    //   ],
+}
